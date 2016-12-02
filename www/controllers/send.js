f21734348e95320425bc06bb20a0947912ac6dbc
yuttpan <yuttpanin@gmail.com>
2016-12-02 07:27:40 +0700

    2/12

--- www/controllers/send.js
+++ www/controllers/send.js
@@ -1,10 +1,28 @@
 angular.module('starter')
 
 .controller('sendController',function ($scope,$http,$state,$stateParams,userService) {
- $scope.an_text = '' ;
-            $scope.results = "result here.";
+ $scope.form = {} ;
+  $scope.btnHome = function (data) {
 
-  $scope.username = window.localStorage['username'] ;
+      $state.go('list');
+    }
+
+            $scope.results = "result here.";
+ $scope.form.date = new Date();
+
+ $scope.cancle = function() {
+	 $scope.form.an_text = "" ;
+
+	 console.log("cancle")
+ }
+
+
+ $scope.send = function(data) {
+	 console.log(data)
+ }
+
+  $scope.user = window.localStorage['username'] ;
+$scope.form.user = $scope.user ;
 console.log(window.localStorage['username']); 
 
             $scope.scan = function () {
@@ -12,7 +30,7 @@ console.log(window.localStorage['username']);
 		            function (result) {
 		                $scope.results = JSON.stringify(result);
 		                $scope.$apply();
-						$scope.an_text = result.text ;
+						$scope.form.an_text = result.text ;
 		            },
 		            function (error) {
 		                alert("Scanning failed: " + error);
